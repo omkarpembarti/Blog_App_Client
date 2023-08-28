4c0963ac0bc3426f6ae677287bcc6090f4926c8f
Omkar Pembarti <omkarpembarti@gmail.com>
2023-08-28 10:34:37 +0530

    Handled incorrect password

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,26 +1,47 @@
 
 import { Button, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import HocCredentialContainer from '../components/HocCredentialContainer'
 import { useNavigate } from 'react-router';
 import { API } from '../Services/api';
+import { UserContext } from '../contexts/UserDataContext';
 // import { userLogin } from '../constants/configs'
 
 const Login = () => {
     const navigate = useNavigate();
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false)
 
+    const { setUserInfo } = useContext(UserContext);
     const onLoginClick = async () => {
         let response = await API.userLogin({ userName, password });
-        console.log(response);
+        if (response.data.msg === 'Incorrect password') {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+            setUserInfo((prevState) => ({ ...prevState, userName }))
+        }
     }
 
     return (
         <HocCredentialContainer >
             <Typography variant='h3'>Blog-App</Typography>
-            <TextField variant='standard' size='small' placeholder='Username' value={userName} onChange={(e) => setUserName(e.target.value)} />
-            <TextField variant='standard' size='small' placeholder='Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <TextField
+                variant='standard'
+                size='small'
+                placeholder='Username'
+
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)} />
+            <TextField
+                error={passwordError}
+                variant='standard'
+                size='small'
+                placeholder='Password'
+                type='password'
+                helperText={`${passwordError ? "Incorrect Password" : ''}`}
+                value={password} onChange={(e) => setPassword(e.target.value)} />
             <Button variant='contained' onClick={onLoginClick}>Login</Button>
             <Typography>OR</Typography>
             <Button
---
+++ src/contexts/UserDataContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react'
+
+export const UserContext = createContext();
+
+const UserDataContext = ({ children }) => {
+    const [userInfo, setUserInfo] = useState({
+        userName: ''
+    });
+    // const setUserInfoWrap = (param) => {
+    //     setUserInfo(param)
+    // }
+    return (
+        <UserContext.Provider value={{ userInfo, setUserInfo }}>
+            {children}
+        </UserContext.Provider>
+
+    )
+}
+
+
+
+export default UserDataContext
